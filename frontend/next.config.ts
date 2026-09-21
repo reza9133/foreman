6f9b6e77bb72470bba07350fcc42f668ba058c8d
amirhossein <119801602+reza9133@users.noreply.github.com>
2026-09-21 23:51:34 +0330

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   reactStrictMode: true,
   turbopack: {},
 };
